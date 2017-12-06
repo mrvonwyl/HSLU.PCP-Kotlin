99c0ddb8d56c28ef117b007e2e86f02a78a6dec2
MarioBucher <mario.b@gmx.ch>
2017-12-07 00:57:27 +0100

    forgot to add the changes to the branch

---
+++ src/Ex8.2.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    val numbers: IntArray = intArrayOf(1, 2, 3, 5, 8)
+    printNumbersAndSquares(numbers)
+}
+
+fun printNumbersAndSquares(numbers: IntArray){
+    val numberlist: List<Int> = numbers.asList()
+    numberlist.forEach{number -> println(number.toString() +": " + number * number)}
+}
---
+++ src/Ex9.1.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+val names: MutableList<String> = mutableListOf("Joe", "Tara", "Sue", "Tim");
+//val x: String? = names
+        //.stream()
+        //.filter((s -> s.startsWith("T)")))
+        //.map(s -> s.toUpperCase())
+        //.reduce
+}
