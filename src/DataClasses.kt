d6a5ff78e91e011d5f3ce445e921e69cd6f4e9e5
Mrvonwyl <mrvonwyl@gmail.com>
2017-12-07 13:59:59 +0100

    DataClass example

--- src/DataClasses.kt
+++ src/DataClasses.kt
@@ -1,9 +1,17 @@
 fun main(args: Array<String>){
     val f1 = Foo("a", 1)
-    val f2 = Foo("b" ,2)
+    val f2 = f1.copy()
     val f3 = f1.copy()
+    f3.a = "b"
 
-    println(f1.equals(f3))
+    // == calls equals
+    // https://stackoverflow.com/questions/45772946/equality-in-kotlin
+    println(f1 == f2)
+    println(f3 == f1)
+
+    // implicit toString()
+    println(f3)
 }
 
-data class Foo(val a: String, val b: Int)
+// var = mutable, val = immutable
+data class Foo(var a: String, val b: Int)
