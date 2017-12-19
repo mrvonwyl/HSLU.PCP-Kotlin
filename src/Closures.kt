1080af39e5278d0c7d4779f9483db18491140f3c
MarioBucher <mario.bucher.01@stud.hslu.ch>
2017-12-19 06:45:40 +0100

    code formatting and bugfixing

--- src/Closures.kt
+++ src/Closures.kt
@@ -3,18 +3,19 @@
 
 fun main(args: Array<String>) {
 
-    val program = ProgramC()
+    val program = Program()
     var result = 0
 
-    val myLambda: (Int, Int) -> Int = {x,y -> x+y}
     program.addTwoNumbers(2,7)  {x,y -> result = x+y}
 
     println(result)
 }
 
-class ProgramC{
+class Program{
     fun addTwoNumbers(a: Int, b:Int, action: (Int, Int) -> Unit){
-        val result =  action(a,b) // result = x + y = a + b = 2 + 7 = 9
-        println("Result of HighLebelFunctions: " + result)
+        action(a,b) // result = x + y = a + b = 2 + 7 = 9
     }
 }
+
+
+
--- src/NullSafety.kt
+++ src/NullSafety.kt
@@ -3,12 +3,11 @@ fun main(args: Array<String>) {
 
     //returns null
     val l = b?.length
-
     // returns default value: 0
     val n = b?.length ?: 0
-
     //throws NPE
-    //val e = b!!.length
+    val e = b!!.length
+
     println(l)
     println(n)
 }
